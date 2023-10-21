79f7e06d70fd5f01050628dafb3e4c775c4bfadd
Peter Joseph <josephpeter489@gmail.com>
2023-10-21 09:42:57 +0100

    continously learning

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { motion } from 'framer-motion/dist/es/index';
 
+import { images } from '../../constants';
 import './Header.scss';
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1]}}
+        transition={{ duration: 0.5 }}
+        className='app__header-info'
+      >
+        <div className='app_header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className='p-text'>Hello, I am</p>
+              <h1 className='head-text'>Michael</h1>
+            </div>
+          </div>
+
+          <div className='tag-cmp app__flex'>
+          <p className='p-text'>Web Developer</p>
+          <p className='p-text'>Freelancer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1]}}
+        transition={{ duration: 0.5, delayChildren:0.5 }}
+        className='app__header-img'
+      >
+        <img src={images.profile} alt='profile_bg' />
+      </motion.div>
+
+      <motion.div>
+        
+      </motion.div>
+    </div>
   )
 }
 
