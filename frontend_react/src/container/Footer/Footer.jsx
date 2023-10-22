d60690492972c3ab93dccec6f65c6a7a2bbfbc8e
Peter Joseph <josephpeter489@gmail.com>
2023-10-23 00:01:29 +0100

    continously learning

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -7,6 +7,36 @@ import './Footer.scss';
 
 
 const Footer = () => {
+
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' })
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { name, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleSubmit = () => {
+    setLoading(true)
+
+    const contact = {
+      _type: 'contact',
+      name: name,
+      email: email,
+      message: message
+    }
+
+    client.create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+  }
+
   return (
     <>
       <h2 className='head-text'>Take a coffee & chat with me</h2>
@@ -22,8 +52,28 @@ const Footer = () => {
         </div>
       </div>
 
-
-
+      {!isFormSubmitted ?
+        <div className='app__footer-form app__flex'>
+          <div className='app__flex'>
+            <input type="text" className='p-text' placeholder='Your Name' name='name' value={name} onChange={handleChangeInput} />
+          </div>
+          <div className='app__flex'>
+            <input type="email" className='p-text' placeholder='Your Email' name='email' value={email} onChange={handleChangeInput} />
+          </div>
+          <div>
+            <textarea 
+              className='p-text'
+              placeholder='Your Message'
+              value={message}
+              name='message'
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type='button' className='p-text' onClick={handleSubmit}>{loading ? 'Sending' : 'Send Message'}</button>
+        </div>
+      : <div>
+        <h3 className='head-text'>Thank you for getting in touch!</h3>
+      </div>}
 
     </>
   )
