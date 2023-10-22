fe8be49875b3f9375fe8786d508bea507a347fc1
Peter Joseph <josephpeter489@gmail.com>
2023-10-22 23:05:07 +0100

    continously learning

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -8,7 +8,24 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <h2 className='head-text'>Take a coffee & chat with me</h2>
+
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt="email" />
+          <a href="mailto:josephpeter489@gmail.com" className='p-text'>josephpeter489@gmail.com</a>
+        </div>
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel: +234 (810) 0335-322" className='p-text'>+234 (810) 0335-322</a>
+        </div>
+      </div>
+
+
+
+
+    </>
   )
 }
 
