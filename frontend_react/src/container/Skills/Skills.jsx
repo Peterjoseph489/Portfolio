ee0a7aaeea3e578cc7501d0212b477b3b98d007d
Peter Joseph <josephpeter489@gmail.com>
2023-10-22 22:56:13 +0100

    continously learning

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -5,39 +5,34 @@ import ReactTooltip from 'react-tooltip';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 
-import './Skills.scss'
-
+import './Skills.scss';
 
 const Skills = () => {
   const [experience, setExperience] = useState([]);
   const [skills, setSkills] = useState([]);
 
-
   useEffect(() => {
     const query = '*[_type == "experiences"]';
     const skillsQuery = '*[_type == "skills"]';
 
-    client.fetch(query)
-      .then((data) => {
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
 
-        setExperience(data);
-      })
-
-    client.fetch(skillsQuery)
-      .then((data) => {
-        setSkills(data);
-      })
-  }, [])
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
 
   return (
     <>
       <h2 className='head-text'>Skills & Experience</h2>
 
       <div className='app__skills-container'>
-        <motion.div className="app__skills-list">
+        <motion.div className='app__skills-list'>
           {skills?.map((skill) => (
             <motion.div
-              whileInView={{opacity: [0, 1]}}
+              whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className='app__skills-item app__flex'
               key={skill.name}
@@ -49,47 +44,43 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
-        <motion.div className="app__skills-exp">
+        <motion.div className='app__skills-exp'>
           {experience?.map((experience) => (
-            <motion.div
-              className='app__skilss-exp-item'
-              key={experience.year}
-            >
+            <motion.div className='app__skilss-exp-item' key={experience.year}>
               <div className='app__skills-exp-year'>
                 <p className='bold-text'>{experience.year}</p>
               </div>
               <motion.div className='app__skills-exp-works'>
-              {experience.works.map((work) => (
-                <>
-                  <motion.div
-                    whileInView={{opacity: [0, 1]}}
-                    transition={{ duration: 0.5 }}
-                    className='app__skills-exp-work'
-                    data-tip
-                    data-for={work.name}
-                    key={work.name}
-                  >
-                    <h4 className='bold-text'>{work.name}</h4>
-                    <p className='p-text'>{work.company}</p>
-                  </motion.div>
-                  <ReactTooltip
-                    id={work.name}
-                    effect='solid'
-                    arrowColor='#fff'
-                    className= 'skills-tooltip'
-                  >
-                    {work.desc}
-                  </ReactTooltip>
-                </>
-              ))}
+                {experience.works.map((work) => (
+                  <div key={work.name}>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className='app__skills-exp-work'
+                      data-tip
+                      data-for={work.name}
+                    >
+                      <h4 className='bold-text'>{work.name}</h4>
+                      <p className='p-text'>{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      effect='solid'
+                      arrowColor='#fff'
+                      className='skills-tooltip'
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </div>
+                ))}
               </motion.div>
             </motion.div>
-          ))}  
+          ))}
         </motion.div>
       </div>
     </>
-  )
-}
+  );
+};
 
 
 export default AppWrap(
