b9100c26c84aecb36089712130e6cdb248b66d2b
Peter Joseph <josephpeter489@gmail.com>
2023-10-22 22:30:26 +0100

    continously learning

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
             </span>
             <div style={{ marginLeft: 20 }}>
               <p className='p-text'>Hello, I am</p>
-              <h1 className='head-text'>Joseph</h1>
+              <h1 className='head-text'>Joseph Peter</h1>
             </div>
           </div>
 
