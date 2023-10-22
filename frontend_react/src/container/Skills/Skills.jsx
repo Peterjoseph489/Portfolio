b516b6b16fa0ec9319e39d18cc7f62695ad2e498
Peter Joseph <josephpeter489@gmail.com>
2023-10-22 14:22:59 +0100

    continously learning

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import ReactTooltip from 'react-tooltip';
-// import { Tooltip as ReactTooltip } from 'react-tooltip'
-// import { Tooltip } from 'react-tooltip'
-// import * as ReactTooltip from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
@@ -95,4 +92,4 @@ const Skills = () => {
   )
 }
 
-export default Skills
+export default AppWrap(Skills, 'skills');
